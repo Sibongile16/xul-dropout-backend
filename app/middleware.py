c0ff620fd32aba465c69f8cfb6fa4e6b70ac6b0b
Sam Banankhu <88266772+Sam-Banankhu@users.noreply.github.com>
2025-06-09 15:25:09 +0200

    Update middleware.py

--- app/middleware.py
+++ app/middleware.py
@@ -5,14 +5,8 @@ origins = [
     "http://localhost:8000",
     "http://127.0.0.1:8000",
     "http://127.0.0.1:3000",
-    "https://tot-35es.onrender.com/",
-    "https://tot-35es.onrender.com"
     "http://localhost:8080",
     "http://localhost:3000",
-    "http://lihess.lighthouse.org.mw/",
-    "http://lihess.lighthouse.org.mw",
-    "http://192.168.3.44:3000",
-    "http://192.168.3.44:8000",
     ]
 
 def add_cors_middleware(app):
